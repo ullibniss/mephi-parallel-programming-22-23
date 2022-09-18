43e3ba3f159b3bf1c2f890c92f32e4c834366b95
Alexey <89032904807alek@gmail.com>
2022-09-18 14:55:15 +0300

    Refactored lab2_parallel

--- lab2/lab2_parallel.c
+++ lab2/lab2_parallel.c
@@ -2,34 +2,45 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <time.h>
-
+#include <stdbool.h>
 int main(int argc, char** argv)
 {
     const int count = 10000000;     ///< Number of array elements
     const int random_seed = time(NULL); ///< RNG seed
-    const int target = 13451;          ///< Number to look for
-
+    const int target = 456789;          ///< Number to look for
+    const int procs = omp_get_num_procs();
     int* array = 0;                 ///< The array we need to f16ind the max in
-    int  index = -1;                ///< The index of the element we need
-
+    long long index;                ///< The index of the element we need
+    bool lock;
     /* Initialize the RNG */
     srand(random_seed);
 
+#ifdef _OPENMP
+    printf("OpenMP: %d\n", _OPENMP);
+#endif
+
     /* Generate the random array */
     array = (int*)malloc(count*sizeof(int));
-    for(int i=0; i<count; i++) { array[i] = rand(); }
+    for(int i=0; i<count; i++) { array[i] = rand() % 10000000; }
 
     /* Find the index of the element */
-#pragma omp parallel default(none) shared(array, count, target) reduction(min:index)
+#pragma omp parallel num_threads(procs) shared(array, count, target) private(lock) reduction(min: index) default(none)
     {
-#pragma for
+        lock = 0;
+
+#pragma omp for
         for (int i = 0; i < count; i++) {
-            if (array[i] == target) {
+            if (array[i] == target && lock != 1) {
+                lock = 1;
                 index = i;
-                break;
             }
         }
-        printf("Local index is %d\n", index);
+
+        if (index > count || index < 0) {
+            index = count;
+            printf("Local index in thread %d not found\n", omp_get_thread_num());
+        }
+        else printf("Local index in thread %d is %d\n", omp_get_thread_num(), index);
     }
     printf("Found occurence of %d at index %d;\n", target, index);
     return(0);
