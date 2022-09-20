db05035cc6cfffbf6b01d0c65f8aba995340e904
Alexey <89032904807alek@gmail.com>
2022-09-21 01:54:57 +0300

    Initial commit for lab3

---
+++ lab3/lab3_parallel.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void shell_sort(int *array, int size) {
+    for (int s = size / 2; s > 0; s /= 2) {
+        for (int i = s; i < size; ++i) {
+            for (int j = i - s; j >= 0 && array[j] > array[j + s]; j -= s) {
+                int temp = array[j];
+                array[j] = array[j + s];
+                array[j + s] = temp;
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    return 0;
+}
